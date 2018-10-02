b569b4c5c265fd8b88076e1af023ceb8e8db39cd
Маздайщик <a.v.konovalov87@mail.ru>
2018-10-02 11:28:11 +0300

    Уточнён скрипт установки

--- setup5.sh
+++ setup5.sh
@@ -4,6 +4,15 @@
   wget -q -O - https://github.com/bmstu-iu9/refal-5-lambda/releases/download/2.2.1/bootstrap-refal-5-lambda-2.2.1.tar.gz | tar xzvf -
   chmod +x bootstrap.sh bin/srefc bin/srmake
   sed -i 's/\r//' *.sh */*.sh bin/srefc bin/srmake
+  echo Compiling sources, wait few minutes...
   ./bootstrap.sh
-  echo 'export PATH=$PATH:~/.local/share/refal-5-lambda/bin' >> ~/.bashrc
+
+  CONFIG=~/.bashrc
+  if [ $(UNAME) == Darwin ]; then
+    CONFIG=~/.bash_profile
+  fi
+  if ! grep -qE 'PATH=.*/.local/share/refal-5-lambda/bin' $CONFIG; then
+    echo 'export PATH=$PATH:~/.local/share/refal-5-lambda/bin' >> $CONFIG
+  fi
+  echo Updated file $CONFIG, check if you need
 )
