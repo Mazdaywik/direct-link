d260f153941984ad9631354afb43228ba15679ec
Маздайщик <a.v.konovalov87@mail.ru>
2020-04-21 14:38:38 +0300

    Оглавление

--- toc.js
+++ toc.js
@@ -1,13 +1,11 @@
 function makeTOC() {
   let tocstr =
-    '\n\n\n> **Table of contents**\n> \n' +
     Array.from(document.querySelectorAll('h1 > a, h2 > a, h3 > a')).map((a) => (
       `<p> ${a.parentNode.tagName} : ${a.parentNode.innerText.trim()} ~ ${a.hash} </p>`
-    )).join('\n') +
-    '\n\n\n'
+    )).join('\n');
 
   let tocdiv = document.getElementById("toc");
-  tocdiv.innerHTML = "<pre>" + tocstr + "</pre>";
+  tocdiv.innerHTML = tocstr;
 }
 
-makeTOC();
+document.body.onload = makeTOC;
