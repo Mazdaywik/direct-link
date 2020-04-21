94a42597b21f8d966c741e69455973dad21d0972
Маздайщик <a.v.konovalov87@mail.ru>
2020-04-21 21:50:33 +0300

    Оглавление

--- toc.js
+++ toc.js
@@ -8,69 +8,64 @@ function makeTOC() {
   let parent = tree;
   let last = null;
 
-  let tocstr =
-    Array.from(document.querySelectorAll('h1 > a, h2 > a, h3 > a, h4 > a, h5 > a')).map((a) => {
-      const h = a.parentNode.tagName;
-      const target = a.hash;
+  Array.from(document.querySelectorAll('h1 > a, h2 > a, h3 > a, h4 > a, h5 > a')).map((a) => {
+    const h = a.parentNode.tagName;
+    const target = a.hash;
 
-      const text = a.parentNode.innerHTML.toString().trim()
-        .replace(/<a [^>]*>/g, "!!")
-        .replace(/<\/a>/g, "??");
-      const level = 0 + h.substr(1);
+    const text = a.parentNode.innerHTML.toString().trim()
+      .replace(/<a [^>]*>/g)
+      .replace(/<\/a>/g);
+    const level = 0 + h.substr(1);
 
-      let diff = "";
+    console.log("h: ", h, "text:", text);
 
-      console.log("h: ", h, "text:", text);
-
-      while (level > prev_level) {
-        diff += "<li><ul>";
-        ++prev_level;
-
-        parent = last;
-        last = {
-          href: parent.href,
-          parent: parent,
-          children: Array.from(parent.children),
-        };
-      }
-
-      while (level < prev_level) {
-        diff += "</ul></li>";
-        --prev_level;
-
-        last = parent;
-        parent = last.parent;
-      }
-
-      const href = `${h} : <a href="${target}">${text}</a>`;
-
-      const node = {
-        href: href,
+    while (level > prev_level) {
+      parent = last;
+      last = {
+        href: parent.href,
         parent: parent,
-        children: [],
+        children: Array.from(parent.children),
       };
-      parent.children.push(node);
-      last = node;
 
-      return diff + `<li>${href}<br></li>`
-    }).join('\n');
+      ++prev_level;
+    }
+
+    while (level < prev_level) {
+      last = parent;
+      parent = last.parent;
+
+      --prev_level;
+    }
+
+    const href = `${h} : <a href="${target}">${text}</a>`;
+
+    const node = {
+      href: href,
+      parent: parent,
+      children: [],
+    };
+    parent.children.push(node);
+    last = node;
+  })
 
   while (prev_level > 1) {
     tocstr += "</ul></li>";
     --prev_level;
   }
 
-  let treestr = (tree) => {
-    let children = tree.children.map(treestr).join('');
+  let childrenstr = (children) => {
+    children = children.map((tree) => (
+      `<li>${tree.href}<br>${childrenstr(tree.children)}</li>`;
+    )).join('');
+
     if (children) {
       children = `<ul>${children}</ul>`;
     }
-    return `<li>${tree.href}<br>${children}</li>`;
-  }
-  tree = treestr(tree);
+  };
+  let list = childrenstr(tree.children);
 
   let tocdiv = document.getElementById("toc");
-  tocdiv.innerHTML = `<ul>${tocstr}</ul> ${tree}`;
+  tocdiv.innerHTML = list;
 }
 
 document.body.onload = makeTOC;
