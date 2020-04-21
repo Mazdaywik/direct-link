ab87f345374f81183fe01e7ee269467516a60a66
Маздайщик <a.v.konovalov87@mail.ru>
2020-04-21 15:18:30 +0300

    Оглавление

--- toc.js
+++ toc.js
@@ -1,29 +1,15 @@
-function replaceAll(str, from, to) {
-  let old;
-
-  do {
-    old = str;
-    str = old.replace(from, to);
-  } while (str != old);
-
-  return str;
-}
-
 function makeTOC() {
   let tocstr =
     Array.from(document.querySelectorAll('h1 > a, h2 > a, h3 > a, h4 > a, h5 > a')).map((a) => {
       let h = a.parentNode.tagName;
       let target = a.hash;
       let text = a.parentNode.innerHTML.trim();
-      let escaped = text;
-      escaped = replaceAll(escaped, '<', "&lt;");
-      escaped = replaceAll(escaped, '>', "&gt;");
 
       let level = 0 + h.substr(1);
 
       return `<li>
         ${h} : <a href="${target}">${text}</a><br>
-        ${escaped}, "${target}", ${level * 10}
+        "${target}", ${level * 10}
       </li>`
     }).join('\n');
 
