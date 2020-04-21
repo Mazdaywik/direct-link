28faca583b5775517c9411cb18605acf483b1218
Маздайщик <a.v.konovalov87@mail.ru>
2020-04-21 14:25:26 +0300

    Оглавление

--- toc.js
+++ toc.js
@@ -1,10 +1,14 @@
-let tocstr =
-  '\n\n\n> **Table of contents**\n> \n' +
-  Array.from(document.querySelectorAll('h1 > a, h2 > a, h3 > a')).map((a) => (
-    {'H1':'> * ','H2':'>   * ','H3':'>     - '}[a.parentNode.tagName] +
-    `[${a.parentNode.innerText.trim()}](${a.hash})`
-  )).join('\n') +
-  '\n\n\n'
+function makeTOC() {
+  let tocstr =
+    '\n\n\n> **Table of contents**\n> \n' +
+    Array.from(document.querySelectorAll('h1 > a, h2 > a, h3 > a')).map((a) => (
+      {'H1':'> * ','H2':'>   * ','H3':'>     - '}[a.parentNode.tagName] +
+      `[${a.parentNode.innerText.trim()}](${a.hash})`
+    )).join('\n') +
+    '\n\n\n'
 
-let tocdiv = document.getElementById("toc");
-tocdiv.innerHTML = tocstr;
+  let tocdiv = document.getElementById("toc");
+  tocdiv.innerHTML = "<pre>" + tocstr + "</pre>";
+}
+
+makeTOC();
