f3028899f4f6c06a226b01e2821b9b0fc4aa281d
Маздайщик <a.v.konovalov87@mail.ru>
2020-04-21 14:59:34 +0300

    Оглавление

--- toc.js
+++ toc.js
@@ -1,6 +1,6 @@
 function makeTOC() {
   let tocstr =
-    Array.from(document.querySelectorAll('h1 > a, h2 > a, h3 > a')).map((a) => (
+    Array.from(document.querySelectorAll('h1 > a, h2 > a, h3 > a')).map((a) => {
       let target = a.hash;
       let text = a.parentNode.innerHTML.trim();
       let escaped = text
@@ -12,7 +12,7 @@ function makeTOC() {
         ${a.parentNode.tagName} : <a href="${target}">${text}</a><br>
         ${escaped}, "${target}"
       </li>`
-    )).join('\n');
+    }).join('\n');
 
   let tocdiv = document.getElementById("toc");
   tocdiv.innerHTML = `<ul>${tocstr}</ul>`;
